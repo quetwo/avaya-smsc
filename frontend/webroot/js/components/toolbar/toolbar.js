7abf1a843ea4e8f0604825446a49f54d662e02f5
Kwiatkowski, Nicholas <nk@msu.edu>
2024-03-01 22:37:10 -0500

    Unread Message count indicator added to toolbar

--- frontend/webroot/js/components/toolbar/toolbar.js
+++ frontend/webroot/js/components/toolbar/toolbar.js
@@ -7,27 +7,29 @@ const toolBarHTML = `
             
             <ul class="nav nav-pills flex-column text-center mb-auto">
                 <li class="nav-item">
-                    <a href="#" id="toolbarHome" class="nav-link py-3 border-bottom border-top rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Home" data-bs-original-title="Home">
+                    <a href="#" id="tbrHome" class="nav-link py-3 border-bottom border-top rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Home" data-bs-original-title="Home">
                         <i class="fa-solid fa-house fa-xl"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="#" id="toolbarYou" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="You" data-bs-original-title="You">
+                    <a href="#" id="tbrYou" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="You" data-bs-original-title="You">
                         <i class="fa-solid fa-circle-user fa-xl"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="#" id="toolbarMessages" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Messages" data-bs-original-title="Messages">
-                        <i class="fa-solid fa-comments fa-xl"></i>
+                    <a href="#" id="tbrMessages" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Messages" data-bs-original-title="Messages">
+                        <i class="fa-solid fa-comments fa-xl position-relative">
+                            <span id="msgIndicator" class="visually-hidden badge rounded-pill bg-danger position-absolute top-0 start-100 translate-middle" style="font-size: 0.40em;">0</span>
+                        </i>
                     </a>
                 </li>
                 <li>
-                    <a href="#" id="toolbarContacts" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Contacts" data-bs-original-title="Contacts">
+                    <a href="#" id="tbrContacts" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Contacts" data-bs-original-title="Contacts">
                         <i class="fa-solid fa-address-book fa-xl"></i>
                     </a>
                 </li>
                 <li>
-                    <a href="#" id="toolbarBookmarks" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Bookmarks" data-bs-original-title="Bookmarks">
+                    <a href="#" id="tbrBookmarks" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Bookmarks" data-bs-original-title="Bookmarks">
                         <i class="fa-solid fa-book-bookmark fa-xl"></i>
                     </a>
                 </li>
@@ -64,18 +66,28 @@ export default class smsToolbar extends HTMLElement
         {
             new bootstrap.Tooltip(tooltipTriggerEl)
         })
+        this.resetActiveList("tbrHome");
     }
 
     static get observedAttributes()
     {
-        return ['active'];
+        return ['active','msgcount'];
     }
 
     attributeChangedCallback(name, oldValue, newValue)
     {
-        if (name === "active")
+        switch(name)
         {
-            this.resetActiveList(newValue);
+            case "active":
+            {
+                this.resetActiveList(newValue);
+                break;
+            }
+            case "msgcount":
+            {
+                this.setUnreadMessages(newValue);
+                break;
+            }
         }
 
     }
@@ -83,13 +95,26 @@ export default class smsToolbar extends HTMLElement
     resetActiveList(newActiveItem)
     {
         const allLinks = Array.from(this.querySelectorAll('.nav-link'));
-        console.log(allLinks);
         allLinks.forEach(link =>
         {
             link.classList.remove("active");
         })
         const newActive = this.querySelector("#"+newActiveItem);
         newActive.classList.add("active");
+    }
+
+    setUnreadMessages(messageCount)
+    {
+        const msgInd = this.querySelector("#msgIndicator");
+        if (messageCount > 0)
+        {
+            msgInd.classList.remove("visually-hidden");
+            msgInd.innerText = messageCount;
+        }
+        else
+        {
+            msgInd.classList.add("visually-hidden");
+        }
     }
 
 }
